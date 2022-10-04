a97133d8ba13118844c303590b8459461b8a0ea9
Dhanush Naidu <64836539+dhanushnaidu77@users.noreply.github.com>
2022-10-04 18:12:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from tokenize import Name
+from webbrowser import get
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+names= {"tim":{"age":19, "gender": "male"},
+        "bill":{"age":32, "gender": "male"},
+        "amy":{"age":25, "gender": "female"}}
+
+class Helloworld(Resource):
+    def get(self, name):
+        return names[name]
+
+      
+
+
+api.add_resource(Helloworld, "/hello/<string:name>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
